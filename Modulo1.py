703b7daf7b6c331680ca519f29ea7e651d340a99
Tiago-Egas <tiagoegas@outook.com>
2026-06-20 11:40:49 -0300

    Modulo 1
    Signed-off-by: Tiago-Egas <tiagoegas@outook.com>

--- Modulo1.py
+++ Modulo1.py
@@ -19,3 +19,22 @@ print("Olá, " + nome + "! Bem-vindo ao Python!")
 idade = int(input("Digite sua idade: "))
 anos = 2026 - idade
 print("Você nasceu em " + str(anos) + ".")
+
+# Aula 4
+telefones = ["1234-5678", "9876-5432", "5555-5555"]
+print(telefones[0])
+telefones.append("1111-1111") # Adiciona um novo telefone ao final da lista
+print(telefones)
+
+telefones.insert(1, "2222-2222") # Insere um novo telefone na posição especificada
+print(telefones)
+
+telefones.remove("9876-5432") # Remove o telefone especificado da lista
+print(telefones)
+
+telefones.pop(0) # Remove o telefone na posição especificada
+print(telefones)
+
+contato = [("João", "1234-5678", "joao@email.com"), ("Maria", "9876-5432", "maria@email.com")] # Lista de contatos, onde cada contato é uma tupla com nome, telefone e email
+print(contato[0]) # Acessa o nome do contato
+print(contato[0][1]) # Acessa o telefone do primeiro contato
