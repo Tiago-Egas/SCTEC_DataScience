2d55913b0faac98f79d3be506dacedaf15a1e7e6
Tiago-Egas <tiagoegas@outook.com>
2026-06-20 10:19:43 -0300

    Modulo 1
    Signed-off-by: Tiago-Egas <tiagoegas@outook.com>

---
+++ Modulo1.py
@@ -0,0 +1,21 @@
+# Aula 1
+print("Meu primeiro programa em Python")
+
+msg="Ola, mundo!"
+print(msg)
+
+# Aula 2
+teste1=1
+teste2=2
+print(teste1+teste2)
+
+teste3 = "Teste 3"
+print(len(teste3))
+
+nome = input("Digite seu nome: ")
+print("Olá, " + nome + "! Bem-vindo ao Python!")
+
+# Aula 3
+idade = int(input("Digite sua idade: "))
+anos = 2026 - idade
+print("Você nasceu em " + str(anos) + ".")
